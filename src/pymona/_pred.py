2f9590960ea8fe3719853bc3443a912f2091b2cc
Neta Elad <netaelad@mail.tau.ac.il>
2025-05-14 11:07:50 +0300

    tmp mode optimization

--- src/pymona/_pred.py
+++ src/pymona/_pred.py
@@ -24,29 +24,30 @@ class LazyPred[**P]:
 
     def __call__(self, *args: P.args, **kwargs: P.kwargs) -> BoolRef:
         if self.pred_ref is None:
-            self.pred_ref = self.build()
+            self.pred_ref = build_pred(self.fun)
 
         return self.pred_ref(*args, **kwargs)  # type: ignore
 
-    def build(self) -> PredRef:
-        name = self.fun.__name__
-        args: list[BoolIdent | ElementIdent | SetIdent] = []
 
-        for param in signature(self.fun).parameters.values():
-            assert (
-                param.kind == Parameter.POSITIONAL_ONLY
-                or param.kind == Parameter.POSITIONAL_OR_KEYWORD
-            )
-            if issubclass(param.annotation, BoolRef):
-                args.append(BoolIdent(param.name))
-            elif issubclass(param.annotation, ElementRef):
-                args.append(ElementIdent(param.name))
-            elif issubclass(param.annotation, SetRef):
-                args.append(SetIdent(param.name))
-            else:
-                assert False, f"Bad parameter {param}"
+def build_pred[**P](fun: TypedPred[P]) -> PredRef:
+    name = fun.__name__
+    args: list[BoolIdent | ElementIdent | SetIdent] = []
 
-        return pred(name, args, self.fun(*args))  # type: ignore
+    for param in signature(fun).parameters.values():
+        assert (
+            param.kind == Parameter.POSITIONAL_ONLY
+            or param.kind == Parameter.POSITIONAL_OR_KEYWORD
+        )
+        if issubclass(param.annotation, BoolRef):
+            args.append(BoolIdent(param.name))
+        elif issubclass(param.annotation, ElementRef):
+            args.append(ElementIdent(param.name))
+        elif issubclass(param.annotation, SetRef):
+            args.append(SetIdent(param.name))
+        else:
+            assert False, f"Bad parameter {param}"
+
+    return pred(name, args, fun(*args))  # type: ignore
 
 
 def to_pred[**P](fun: TypedPred[P]) -> TypedPred[P]:
